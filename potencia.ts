20697e4afa232e24542467105af054d1c8cb06eb
lucascolantonio <127357694+lucascolantonio@users.noreply.github.com>
2023-05-11 19:18:57 -0300

    Add files via upload

--- potencia.ts
+++ potencia.ts
@@ -1,11 +1,13 @@
 import * as rls from "readline-sync";
 let base: number = rls.questionInt ("ingrese la baase")
 let exponente: number = rls.questionInt("ingrese el exponente")
-    while(exponente<=0){exponente=rls.questionInt("por favor el numero exponente debe ser mayor a 0 ")}
-     if (exponente>=1){calcularpotencia}
-function calcularpotencia(base, exponente){    
+ let resultado: number=0
+while(exponente< 0){exponente=rls.questionInt("por favor el numero exponente debe ser mayor a 0 ")}
+     if (exponente>0){calcularpotencia(base, exponente)}
+     else (exponente ===0);{resultado= 1 }
+function calcularpotencia(base: number, exponente: number):number{    
 let potencia:number=base**exponente
 return potencia
 }
-let resultado= calcularpotencia(base,exponente)
+ resultado= calcularpotencia(base,exponente)
 console.log("el resultado es:", resultado)
