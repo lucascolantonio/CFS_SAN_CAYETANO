8b387f4eee5b6eb9832c09008054d4db5b28801a
lucascolantonio <127357694+lucascolantonio@users.noreply.github.com>
2023-06-03 10:25:46 -0300

    Actualizar multiplos.ts

--- multiplos.ts
+++ multiplos.ts
@@ -1,5 +1,5 @@
 let resultado: string = "";
-for (let a : number = 1 ; a <= 100 ; a +1) {
+for (let a : number = 1 ; a <= 100 ; a++) {
      if ( a % 2 === 0 || a % 3 === 0){
     resultado = resultado + a + " ";
     }
